0ae59a8fdcefb46d79ecfcc7fe45ae902f549dbc
jgparsons <jgp67@georgetown.edu>
2025-12-11 09:30:06 -0500

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,41 @@
+# app/main.py
+
+import os
+import datetime
+from app.collect_FRED_data import collect_FRED_data
+from app.send_emails import send_email_to_list
+
+MAILING_LIST = os.getenv("MAILING_LIST")
+
+def main():
+    print("Running daily email job...")
+
+    try:
+        (
+            onrrp,
+            effr,
+            iorb,
+            sofr,
+            srf,
+            _svg_bytes,   # unused now
+            _effr_date,
+            _effr_label,
+        ) = collect_FRED_data()
+
+        send_email_to_list(
+            mailing_list_address=MAILING_LIST,
+            onrrp_today=onrrp,
+            effr_today=effr,
+            iorb_today=iorb,
+            sofr_today=sofr,
+            srf_today=srf,
+            subject="Daily Fed Rate Update",
+        )
+
+        print("Daily rate email sent successfully!")
+    except Exception as e:
+        print("ERROR running daily email job:", e)
+
+
+if __name__ == "__main__":
+    main()
